d67afba5b46ee1877d44f643ec23c16204c0264b
Alsegurarp <163333582+Alsegurarp@users.noreply.github.com>
2024-11-12 14:55:42 +0100

    No funciona metodo delete

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import dotenv from "dotenv";
 dotenv.config(); //Lee el fichero .env y crea las variables de entorno - Esta leyendo que el puerto es el 4000 (Declarado en el fichero)
 //--------
 import express from "express";
-import { leerTareas } from "./db.js";
+import { leerTareas, crearTareas, borrarTareas } from "./db.js";
 
 const servidor = express();
 //invocar express
+
+servidor.use((peticion, respuesta, siguiente) => 
+    {console.log("entramos al middleware");
+    siguiente();
+   //Entro al middleware, hago lo que debo de hacer pero no respondo, sino respondo entonces el proceso no ha terminado
+   //Una vez que he terminado debo invocar la function siguiente() - Una vez que termine te sales del middleware
+})
 
 servidor.use(express.json());
 
@@ -30,12 +37,34 @@ try{
 })
 //Si la URL es /tareas, entonces haré ésto.
 
-servidor.post("/tareas/nueva", (peticion, respuesta) => {
-console.log(peticion.body);
-respuesta.send("sdjlskajdksal");
+servidor.post("/tareas/nueva", async (peticion, respuesta, siguiente) => {
+let {tarea} = peticion.body;
+//Si peticion.body contiene algo, entonces pasa
+if(!tarea || tarea.trim() == ""){
+    return siguiente(true)
+}
+try{
+    let id = await crearTareas(tarea);
+    //tarea viene del cuerpo de la peticion que fue enviada
+    respuesta.json({id});
+    //Estamos mandando un objeto con la propiedad 'id'
+}catch(error){
+    respuesta.status(500);
+    respuesta.json({error : "Error en el servidor"})
+}
 })
 
+servidor.delete("/tareas/borrar/:id", async (peticion, respuesta) => {
+    respuesta.send("x cosa" + peticion.params.id);
+    //Haces una peticion con delete y despues te debe de responder esto
+    //.params.id para que el servidor pueda leer el contenido que es dinámico.
+})
 
+servidor.use((error, peticion, respuesta, siguiente) => {
+    respuesta.status(400);
+    respuesta.json({error : "error en la peticion "});
+});
+//Middleware de gestor de errores - 
 
 
 
